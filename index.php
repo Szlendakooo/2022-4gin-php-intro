7b57546908753841201469e8ceadfc38dacd646d
Szlendakooo <95409714+Szlendakooo@users.noreply.github.com>
2022-09-26 21:57:23 +0200

    2.1

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
     $ipsum="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut enim odio, vehicula ac magna vel, viverra lacinia nisl. Etiam nec massa enim. Morbi sed dictum nisi. Suspendisse et gravida ipsum, eu accumsan mi. Ut bibendum nisi sapien, ac mollis ex elementum vel. Integer metus nulla, efficitur quis blandit in, molestie sed sem. Nullam eget massa turpis. Suspendisse turpis leo, consectetur sed massa ac, viverra cursus lorem. Quisque vel nibh efficitur, placerat ante et, fringilla nulla. Suspendisse urna velit, tempor eget odio at, commodo venenatis neque. Donec vestibulum mollis diam eu egestas. Quisque iaculis lorem eros, et interdum justo pellentesque vitae.";
     $slowa=explode(" ",$ipsum);
-    var_dump($slowa);
+    foreach($slowa as $slowo){
+        if(strstr($slowo,'e')==true){
+            $filtr[]=$slowo;
+        }
+    }
+    var_dump($filtr);
 ?>
