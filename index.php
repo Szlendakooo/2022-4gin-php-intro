22cf9769c0c6b7f7546fd4640a5e4a7e1ec20236
Szlendakooo <95409714+Szlendakooo@users.noreply.github.com>
2022-09-26 22:28:43 +0200

    3.3

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
     $ipsum="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut enim odio, vehicula ac magna vel, viverra lacinia nisl. Etiam nec massa enim. Morbi sed dictum nisi. Suspendisse et gravida ipsum, eu accumsan mi. Ut bibendum nisi sapien, ac mollis ex elementum vel. Integer metus nulla, efficitur quis blandit in, molestie sed sem. Nullam eget massa turpis. Suspendisse turpis leo, consectetur sed massa ac, viverra cursus lorem. Quisque vel nibh efficitur, placerat ante et, fringilla nulla. Suspendisse urna velit, tempor eget odio at, commodo venenatis neque. Donec vestibulum mollis diam eu egestas. Quisque iaculis lorem eros, et interdum justo pellentesque vitae.";
     $szukane="";
+    $liczba_kolumn=6;
     function filtruj($tablica,$szukaj){
     $slowa=explode(" ",$tablica);
     sort($slowa,SORT_NATURAL| SORT_FLAG_CASE);
@@ -12,10 +13,11 @@
         return $filtr;
     }
     var_dump(filtruj($ipsum,$szukane));
+    function renderHTMLTable($tablica,$kolumny){
     $table='<table>';
-    $slowa=explode(" ",$ipsum);
+    $slowa=explode(" ",$tablica);
     $liczba=0;
-    $kolumny=4;
+    $licz_kolumny=$kolumny-1;
     foreach($slowa as $slowo){
         if(
             $liczba%$kolumny==0
@@ -30,7 +32,7 @@
             $table .='<td>'.$slowo.'</td>';
         }
         if(
-            $liczba%$kolumny==$kolumny-1
+            $liczba%$kolumny==$licz_kolumny
         ){
             $table .='</tr>';
         }
@@ -43,5 +45,7 @@
     ){
         $table .='</tr>'; 
     }
-    echo $table;
+    return $table;
+    }
+    echo renderHTMLTable($ipsum,$liczba_kolumn);
 ?>
